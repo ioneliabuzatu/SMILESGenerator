2c5c11080354401a7685ae792fd490f219eafe94
ioneliabuzatu <buzatuionelia@gmail.com>
2021-05-25 15:05:59 +0200

    [CLUSTER] cluster_smiles

--- utils/parallel_generation_smiles.sh
+++ utils/parallel_generation_smiles.sh
@@ -1,3 +1,5 @@
 echo $PWD
+rm /home/mila/g/golemofl/data/smiles-project/my_smiles.txt
+echo "Removed previous submission smiles '/home/mila/g/golemofl/data/smiles-project/my_smiles.txt'"
 python utils/generate_smiles.py
 python utils/generate_smiles.py
